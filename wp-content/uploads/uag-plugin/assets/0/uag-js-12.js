b42f5033113d5a61f20d0fb0064bcd658f7f284a
Karan Gurdittaa <karan608@gmail.com>
2024-06-09 17:25:08 +0530

    assadasd

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-12.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-7d707187' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-b43afb32' );
+});
+ });
